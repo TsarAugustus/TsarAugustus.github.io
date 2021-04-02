3737c892fc577d4372ad40742ff134d95850dceb
John Ostap <ostap.john@gmail.com>
2021-04-02 11:04:44 -0700

    More styling for desktop, fixng how things work

--- idle/js/returnButtonInfo.js
+++ idle/js/returnButtonInfo.js
@@ -1,7 +1,7 @@
 export function returnButtonInfo(skill, init) {
     const newSkillButton = {};
     newSkillButton.id = `${skill.displayName}Button`;
-    newSkillButton.innerHTML = `${skill.displayName}<br> 
+    newSkillButton.innerHTML = `<b>${skill.displayName}</b><br> 
                                 Level: ${skill.level}<br>
                                 ${skill.currentXP}/${skill.XPToLevel}`;
     
--- idle/js/skills/Woodcrafting.js
+++ idle/js/skills/Woodcrafting.js
@@ -8,51 +8,62 @@ export let Woodcrafting = {
     level: 0,
     currentXP: 0,
     XPToLevel: 100,
-    category: 'Crafting',
+    category: 'Basic',
     requirements: {
         item: { Wood: 1 }
     },
     onclick: function() {
+        const interaction = document.getElementById('interaction')
         const craftableItems = {
             Log: { Wood: 2 },
             Plank: { Wood: 2 },
             Chair: { Log: 4, Plank: 2 } 
         }
 
-        const interaction = document.getElementById('interaction');
-        const craftKeys = Object.keys(craftableItems);
-        interaction.innerHTML = '';
-        for(let i=0; i<craftKeys.length; i++) {
-            if(!document.getElementById(craftKeys[i])) {
-                const thisCraftItem = document.createElement('button');
-                thisCraftItem.id = craftKeys[i];
-                thisCraftItem.innerHTML = `${craftKeys[i]}<br>`;
+        this.classList.toggle('ViewedSkill');
+        if(!this.classList.contains('ViewedSkill')) {
+            //should make this redundant
+            while(interaction.lastChild) {
+                interaction.removeChild(interaction.lastChild);
+            }
+        } else {
+            const craftKeys = Object.keys(craftableItems);
+            //should make this redundant
+            while(interaction.lastChild) {
+                interaction.removeChild(interaction.lastChild);
+            }
+            for(let i=0; i<craftKeys.length; i++) {
+                if(!document.getElementById(craftKeys[i])) {
+                    const thisCraftItem = document.createElement('button');
+                    thisCraftItem.id = craftKeys[i];
+                    thisCraftItem.innerHTML = `<b>${craftKeys[i]}</b><br>`;
 
-                for(let j=0; j<Object.keys(craftableItems[craftKeys[i]]).length; j++) {
-                    thisCraftItem.innerHTML += `${Object.keys(craftableItems[craftKeys[i]])[j]}:
-                                                ${Object.values(craftableItems[craftKeys[i]])[j]}<br>`;
-                }
-                thisCraftItem.onclick = function() {
-                    let craftCounter = 0;
-                    const craftKey = Object.keys(craftableItems[craftKeys[i]]);
-                    const craftValue = Object.values(craftableItems[craftKeys[i]]);
-                    for(let j=0; j<craftKey.length; j++) {
-                        if(Player.inventory[craftKey[j]] && Player.inventory[craftKey[j]].amount >= craftValue[j]) {
-                            craftCounter++;
-                        }
+                    for(let j=0; j<Object.keys(craftableItems[craftKeys[i]]).length; j++) {
+                        thisCraftItem.innerHTML += `${Object.keys(craftableItems[craftKeys[i]])[j]}:
+                                                    ${Object.values(craftableItems[craftKeys[i]])[j]}<br>`;
                     }
-                    if(craftCounter === craftKey.length) {
+                    thisCraftItem.onclick = function() {
+                        let craftCounter = 0;
+                        const craftKey = Object.keys(craftableItems[craftKeys[i]]);
+                        const craftValue = Object.values(craftableItems[craftKeys[i]]);
                         for(let j=0; j<craftKey.length; j++) {
-                            Player.inventory[craftKey[j]].amount -= craftValue[j];
+                            if(Player.inventory[craftKey[j]] && Player.inventory[craftKey[j]].amount >= craftValue[j]) {
+                                craftCounter++;
+                            }
                         }
-                        if(!Player.inventory[craftKeys[i]]) {
-                            Player.inventory[craftKeys[i]] = { amount: 0 }
+                        if(craftCounter === craftKey.length) {
+                            for(let j=0; j<craftKey.length; j++) {
+                                Player.inventory[craftKey[j]].amount -= craftValue[j];
+                            }
+                            if(!Player.inventory[craftKeys[i]]) {
+                                Player.inventory[craftKeys[i]] = { amount: 0 }
+                            }
+                            Player.inventory[craftKeys[i]].amount++;
+                            updateInventory();
                         }
-                        Player.inventory[craftKeys[i]].amount++;
-                        updateInventory();
                     }
+                    interaction.appendChild(thisCraftItem)
                 }
-                interaction.appendChild(thisCraftItem)
             }
         }
     }
